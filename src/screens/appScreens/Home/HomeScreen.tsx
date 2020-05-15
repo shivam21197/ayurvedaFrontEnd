6c14551159042e2b344f01b967af9b4c428c8b01
Shivam <shivam.pandey@nineleaps.com>
2020-05-15 15:03:33 +0530

    added search text input

--- src/screens/appScreens/Home/HomeScreen.tsx
+++ src/screens/appScreens/Home/HomeScreen.tsx
@@ -1,168 +1,48 @@
-import Card from '@components/molecules/Card';
-import CardList from '@components/organisms/CardList';
 import StateAwareComponent from '@components/organisms/StateAwareComponent';
-import {ICampaignRequest} from '@domain/interfaces';
-import HomeActions from '@modules/home/actions';
-import HomeSelector from '@modules/home/selectors';
-import {IState} from '@modules/interfaces';
-import RoutesNames from '@navigation/routes';
 import {theme} from '@styles/theme';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, TextInput} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {NavigationScreenProp, NavigationState} from 'react-navigation';
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import {Text} from '@components/atoms/Text';
+import {ScrollView} from 'react-native-gesture-handler';
 
-interface IHomeProps {
-  navigation: NavigationScreenProp<NavigationState>;
-  getHomeFeeds: (pageNumbe: number) => void;
-  feeds: ICampaignRequest[] | null;
-  isFeedsLoading: boolean;
-  isFeedsError: string;
-}
-
-// TODO: add on scroll down to call the api to fetch more feeds pasing the page count
-
-interface IHomeState {
-  page: number;
-}
-
-class HomeScreen extends React.PureComponent<IHomeProps, IHomeState> {
-  constructor(props: IHomeProps) {
-    super(props);
-    this.state = {
-      page: 1,
-    };
-  }
-
-  componentDidMount() {
-    this.getHomeFeeds();
-  }
-
-  public getHomeFeeds = (): void => {
-    const {getHomeFeeds} = this.props;
-    const {page} = this.state;
-    getHomeFeeds(page);
-  };
-
+class HomeScreen extends React.PureComponent<any, any> {
   render() {
-    const {isFeedsLoading, isFeedsError} = this.props;
     return (
       <SafeAreaView style={[styles.container, styles.flexOne]}>
         <StateAwareComponent
-          loading={isFeedsLoading}
-          error={isFeedsError}
+          loading={false}
+          error={''}
           renderComponent={this.renderScreen()}
-          onErrorPress={this.getHomeFeeds}
+          onErrorPress={() => {}}
         />
       </SafeAreaView>
     );
   }
 
   public renderScreen = (): React.ReactNode => {
-    const {feeds} = this.props;
-    if (!feeds) {
-      return null;
-    }
-
     return (
-      <View style={[styles.screenConatiner, styles.flexOne]}>
-        <CardList
-          data={feeds}
-          isHorizontal={false}
-          renderItem={this.renderHomeCard}
-          cardListkey="homeCampaigns"
-        />
-      </View>
+      <ScrollView
+        contentContainerStyle={[styles.flexOne, styles.screenConatiner]}
+        keyboardShouldPersistTaps="handled"
+        keyboardDismissMode="on-drag"
+        showsHorizontalScrollIndicator={false}>
+        <Text>{'this is text screen'}</Text>
+        <TextInput value={'search'} />
+      </ScrollView>
     );
-  };
-
-  public renderHomeCard = ({
-    item,
-    index,
-  }: {
-    item: ICampaignRequest;
-    index: number;
-  }): React.ReactElement => {
-    const {
-      _id,
-      title,
-      subTitle,
-      status,
-      description,
-      thumbnails,
-      creatorId,
-      donerIds,
-      groupMemberIds,
-    } = item;
-
-    const onCampaignPress = () => {
-      const {navigation} = this.props;
-      navigation.navigate(RoutesNames.HomeCampaignDescriptionScreen, {
-        _id,
-        screenName: RoutesNames.HomeScreen,
-      });
-    };
-
-    return (
-      <Card
-        _id={_id}
-        title={title}
-        status={status}
-        subTitle={subTitle}
-        description={description}
-        thumbnails={thumbnails}
-        creatorId={creatorId}
-        donerIds={donerIds}
-        groupMemberIds={groupMemberIds}
-        cardIndex={index}
-        onCardPress={onCampaignPress}
-        onMemberViewAll={this.onMemberViewAll}
-        onDonerViewAll={this.onDonerViewAll}
-      />
-    );
-  };
-
-  // TODO: add nav
-
-  public onMemberViewAll = () => {};
-  public onDonerViewAll = () => {};
-
-  public onCampaignPress = () => {
-    const {navigation} = this.props;
-    navigation.navigate(RoutesNames.HomeCampaignDescriptionScreen, {});
   };
 }
 
-const mapStateToProps = (state: IState) => {
-  const {getFeedsError, getFeedsLoading, getHomeFeeds} = HomeSelector;
-  return {
-    feeds: getHomeFeeds(state),
-    isFeedsLoading: getFeedsLoading(state),
-    isFeedError: getFeedsError(state),
-  };
-};
-
-const mapDispatchToProps = (dispatch: any) => {
-  const {getHomeFeeds} = HomeActions;
-
-  return bindActionCreators(
-    {
-      getHomeFeeds,
-    },
-    dispatch,
-  );
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
+export default connect(null, null)(HomeScreen);
 
 const styles = StyleSheet.create({
   flexOne: {
     flex: 1,
   },
   container: {
-    backgroundColor: theme.colors.raisinBlack,
+    backgroundColor: theme.colors.white,
     borderBottomColor: theme.colors.white,
     borderBottomWidth: theme.layout.screenBottomBorderWidth,
   },
